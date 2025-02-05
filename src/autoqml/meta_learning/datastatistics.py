15420b23e12364944228f88874f9d0a151001558
Moritz Willmann <moritz.willmann@ipa.fraunhofer.de>
2025-02-05 09:53:21 +0100

    remove unnecessary meta learning

--- src/autoqml/meta_learning/datastatistics.py
+++ src/autoqml/meta_learning/datastatistics.py
@@ -1,8 +1,6 @@
 import abc
 from dataclasses import dataclass
 
-from autoqml.constants import InputData
-
 
 @dataclass
 class DataStatistics(abc.ABC):
@@ -10,21 +8,6 @@ class DataStatistics(abc.ABC):
     n_features: int
 
 
-class FeatureExtractor(abc.ABC):
-    def __init__(self):
-        pass
-
-    @abc.abstractmethod
-    def calculate(self, X: InputData) -> DataStatistics:
-        pass
-
-
 @dataclass
 class TabularStatistics(DataStatistics):
     pass
-
-
-class TabularFeatureExtractor(FeatureExtractor):
-    def calculate(self, X: InputData) -> TabularStatistics:
-        # 1. Extract meta-features from InputData
-        pass
