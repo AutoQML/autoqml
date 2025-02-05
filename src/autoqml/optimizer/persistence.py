6c8a6c74faae0a9a7f5c17ec07419aca3399eff6
Moritz Willmann <moritz.willmann@ipa.fraunhofer.de>
2025-02-05 10:22:58 +0100

    delete persistence

--- src/autoqml/optimizer/persistence.py
+++
@@ -1,7 +0,0 @@
-from autoqml.optimizer.evaluation import Trial
-
-
-class Persistence:
-    def record_trial(self, trial: Trial):
-        # TODO: store information provided by sQuLearn and Trial
-        pass
